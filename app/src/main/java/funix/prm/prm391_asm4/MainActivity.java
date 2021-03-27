215c83df8722b38e45e0b0be3df44ba0e24aa39b
Minh Ho <61931543+minhho89@users.noreply.github.com>
2021-03-27 10:54:00 +0700

    feature added bottomNavigation changes moves and profiles fragments

--- app/src/main/java/funix/prm/prm391_asm4/MainActivity.java
+++ app/src/main/java/funix/prm/prm391_asm4/MainActivity.java
@@ -1,14 +1,53 @@
 package funix.prm.prm391_asm4;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+    BottomNavigationView bottomNav;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bottomNav = findViewById(R.id.bottomNavigationView);
+        bottomNav.setOnNavigationItemSelectedListener(navListener);
+
+        getSupportFragmentManager()
+                .beginTransaction()
+                .add(R.id.fragment,
+                        new MoviesFragment())
+                .commit();
     }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    Fragment selectedFragment = null;
+                    switch (item.getItemId()) {
+                        case R.id.botNav_movies:
+                            selectedFragment = new MoviesFragment();
+                            break;
+                        case R.id.botNav_profile:
+                            selectedFragment = new ProfileFragment();
+                            break;
+                    }
+
+                    getSupportFragmentManager()
+                            .beginTransaction()
+                            .replace(R.id.fragment, selectedFragment)
+                            .commit();
+
+                    return true;
+                }
+            };
+
 }
