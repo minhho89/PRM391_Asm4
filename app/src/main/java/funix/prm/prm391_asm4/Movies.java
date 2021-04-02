10664c2a36e2dd3dfc79e3e376b12f2af5e6698d
Minh Ho <61931543+minhho89@users.noreply.github.com>
2021-04-02 22:38:57 +0700

    layout added, recyclerView added

---
+++ app/src/main/java/funix/prm/prm391_asm4/Movies.java
@@ -0,0 +1,40 @@
+package funix.prm.prm391_asm4;
+
+public class Movies {
+    private String mImageLink;
+    private String mMoviesName;
+    private String mMoviePrice;
+
+    public Movies() {
+    }
+
+    public Movies(String mImageLink, String mMoviesName, String mMoviePrice) {
+        this.mImageLink = mImageLink;
+        this.mMoviesName = mMoviesName;
+        this.mMoviePrice = mMoviePrice;
+    }
+
+    public String getmImageLink() {
+        return mImageLink;
+    }
+
+    public void setmImageLink(String mImageLink) {
+        this.mImageLink = mImageLink;
+    }
+
+    public String getmMoviesName() {
+        return mMoviesName;
+    }
+
+    public void setmMoviesName(String mMoviesName) {
+        this.mMoviesName = mMoviesName;
+    }
+
+    public String getmMoviePrice() {
+        return mMoviePrice;
+    }
+
+    public void setmMoviePrice(String mMoviePrice) {
+        this.mMoviePrice = mMoviePrice;
+    }
+}
--- app/src/main/java/funix/prm/prm391_asm4/MoviesFragment.java
+++ app/src/main/java/funix/prm/prm391_asm4/MoviesFragment.java
@@ -1,13 +1,7 @@
 package funix.prm.prm391_asm4;
 
-import android.app.ActionBar;
 import android.os.Bundle;
-
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.appcompat.widget.Toolbar;
-import androidx.fragment.app.Fragment;
-
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -16,21 +10,34 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Toast;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link MoviesFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import org.json.JSONArray;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
 public class MoviesFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    private static final String URL = "https://api.androidhive.info/json/movies_2017.json";
+    private RecyclerView mRecyclerView;
+    private MoviesAdapter mAdapter;
+    private ArrayList<Movies> mMoviesList;
+    private RequestQueue mRequestQueue;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
     public MoviesFragment() {
         // Required empty public constructor
@@ -55,30 +62,12 @@ public class MoviesFragment extends Fragment {
         return super.onOptionsItemSelected(item);
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment MoviesFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static MoviesFragment newInstance(String param1, String param2) {
-        MoviesFragment fragment = new MoviesFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setHasOptionsMenu(true);
     }
-
 
     // Change title
     @Override
@@ -88,8 +77,56 @@ public class MoviesFragment extends Fragment {
         getActivity().setTitle(R.string.movies_text);
 
         View rootView = inflater.inflate(R.layout.fragment_movies, container, false);
+
+        mMoviesList = new ArrayList<>();
+        mAdapter = new MoviesAdapter(getActivity(), mMoviesList);
+        mRecyclerView = rootView.findViewById(R.id.recycler_view);
+
+        RecyclerView.LayoutManager mLayoutManager = new GridLayoutManager(getActivity(), 3);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+        mRecyclerView.setItemAnimator(new DefaultItemAnimator());
+        mRecyclerView.setAdapter(mAdapter);
+        mRecyclerView.setNestedScrollingEnabled(false);
+
+        fetchMoviesItem();
+
+
         return rootView;
 
+    }
+
+    private void fetchMoviesItem() {
+        JsonArrayRequest request = new JsonArrayRequest(URL,
+                new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        if (response == null) {
+                            Toast.makeText(getActivity(), "Couldn't fetch movies! Pleas try again.",
+                                    Toast.LENGTH_LONG).show();
+                            return;
+                        }
+
+                        List<Movies> movie = new Gson().fromJson(response.toString(),
+                                new TypeToken<List<Movies>>() {
+                                }.getType());
+
+                        mMoviesList.clear();
+                        mMoviesList.addAll(movie);
+
+                        // update recyclerView
+                        mAdapter.notifyDataSetChanged();
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                // error in getting json
+                Log.e("FetchMoviesItem", "Error: " + error.getMessage());
+                Toast.makeText(getActivity(), "Error: " + error.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        MainActivity.getInstance().addToRequestQueue(request);
     }
 
 
